3ccabfc1c0beef9c0cc2a04dcaab6debfe29c52f
aborasemh18-spec <a.borase.mh18@gmail.com>
2026-07-01 11:13:07 +0530

    sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../components/shared/sidebar/sidebar.component';
+import { HeaderComponent } from '../components/shared/header/header.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>'
+  imports: [RouterOutlet, CommonModule, SidebarComponent, HeaderComponent],
+  templateUrl: './app.html'
 })
-export class App {}
+export class App {
+  authService = inject(AuthService);
+  router = inject(Router);
+  currentUser = this.authService.currentUser;
+  
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
+
